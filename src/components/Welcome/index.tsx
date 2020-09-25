7031d1568ab99f479a5c92585570d827f1d838db
hiagopsilva <hiagohps360@gmail.com>
2020-09-25 19:29:45 -0300

    chore: remove welcome component

--- src/components/Welcome/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import {Text} from './styles';
-
-const Welcome: React.FC = () => {
-  return <Text>Welcome, Template Mobile!</Text>;
-};
-
-export default Welcome;
--- src/components/Welcome/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components/native';
-
-export const Text = styled.Text`
-  font-family: sans-serif;
-  font-weight: bold;
-  font-size: 24px;
-  color: #fff;
-`;
